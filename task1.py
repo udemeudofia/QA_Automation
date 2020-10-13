b0d3d1e2e3a8e2aa549d4c38a3ca74486b772fd8
udemeudofia <udemeudofia01@gmail.com>
2020-10-13 12:32:09 +0300

    Updated changes

--- task1.py
+++ task1.py
@@ -2,21 +2,22 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+
 print('Test case started ....')
 # driver = webdriver.Chrome(r"C:\Users\Udeme\PycharmProjects\QA_Engineer\Browsers\chromedriver.exe")
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
 driver.get('https://www.tiketa.lt/EN/search')
 driver.find_element_by_name('sf_TextFilter').send_keys('Forum')
-time.sleep(3)
+time.sleep(2)
 driver.find_element_by_xpath("//*[@id='dropdownMenu3']").send_keys(Keys.ARROW_DOWN)
-time.sleep(3)
+time.sleep(2)
 driver.find_element_by_xpath("//*[@id='search-city']/div[2]/div/div/div/div/ul/li[3]/a[text()='Kaunas']").click()
-time.sleep(3)
+time.sleep(2)
 driver.find_element_by_name('sf_DateFrom').send_keys('2020-09-01')
 driver.find_element_by_name('sf_DateTo').send_keys('2021-12-31')
-time.sleep(3)
+time.sleep(2)
 driver.find_element_by_css_selector('button.bn.btn-lg.btn-red.text-uppercase').send_keys(Keys.ENTER)
-time.sleep(10)
+time.sleep(5)
 driver.close()
 print('Test case finished ....')
--- task3.py
+++ task3.py
@@ -8,15 +8,15 @@ driver.maximize_window()
 driver.get('http://the-internet.herokuapp.com/challenging_dom')
 time.sleep(3)
 
-def highlight(element, effect_time, color, border):
-    """Highlights (blinks) a Selenium Webdriver element"""
+def to_highlight(element, duration, color, border_size):
+    """Highlights a web element"""
     driver = element._parent
     def apply_style(s):
         driver.execute_script("arguments[0].setAttribute('style', arguments[1]);",
                               element, s)
     original_style = element.get_attribute('style')
-    apply_style("border: {0}px solid {1};".format(border, color))
-    time.sleep(effect_time)
+    apply_style("border: {0}px solid {1};".format(border_size, color))
+    time.sleep(duration)
     apply_style(original_style)
 
 table_path = "//*[@id='content']/div/div/div/div[2]/table/tbody"
@@ -26,11 +26,11 @@ t3 = driver.find_element_by_xpath(f"{table_path}/tr[3]/td[7]/a[1]")
 t4 = driver.find_element_by_xpath(f"{table_path}/tr[8]/td[4]")
 t5 = driver.find_element_by_xpath(f"{table_path}/tr[8]/td[1]")
 
-highlight(t1, 2, 'red', 5)
-highlight(t2, 2, 'red', 5)
-highlight(t3, 2, 'red', 5)
-highlight(t4, 2, 'red', 5)
-highlight(t5, 2, 'red', 5)
+to_highlight(t1, 2, 'red', 5)
+to_highlight(t2, 2, 'red', 5)
+to_highlight(t3, 2, 'red', 5)
+to_highlight(t4, 2, 'red', 5)
+to_highlight(t5, 2, 'red', 5)
 # time.sleep(2)
 driver.find_element_by_css_selector('a.button.success').send_keys(Keys.ENTER)
 
