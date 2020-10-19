ad4beadaed089c91d8477536bc489b7e69db8475
Udeme Udofia <udemeudofia01@gmail.com>
2020-10-19 15:01:42 +0300

    Adding conftest.py file

---
+++ task/test/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope='session')
+def setup():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield driver
+    driver.close()
